06a7a3a84bf9174425919aed754968cf04e2f199
Joachim Rives <47552793+SS-Runen@users.noreply.github.com>
2021-12-02 15:39:47 +0800

    Added boilerplate configuration for ValidationPipes in main.ts. Added code for taking configuration from .env file in app.module.ts. Track tsconfig.build.json.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.${process.env.STAGE}`],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true,
+        logging: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
