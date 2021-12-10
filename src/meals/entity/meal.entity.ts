51db5b493b209a1ece126b694bd0e3b3399e1b78
Joachim Rives <47552793+SS-Runen@users.noreply.github.com>
2021-12-10 19:32:09 +0800

    Temporary working demonstration to query meals by keyword. Meals with names like keyword, regardless of case (ILIKE), or with ingredients with case-sensitive match are selected.

--- src/meals/entity/meal.entity.ts
+++ src/meals/entity/meal.entity.ts
@@ -1,34 +1,34 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('meals')
 export class MealEntity {
-    @PrimaryGeneratedColumn('uuid')
-    id:string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column()
-    userId:string;
+  @Column()
+  userId: string;
 
-    @Column()
-    mealName:string;
+  @Column()
+  mealname: string;
 
-    @Column()
-    mealImage:string;
+  @Column()
+  mealImage: string;
 
-    @Column()
-    recipe:string;
+  @Column()
+  recipe: string;
 
-    @Column({array:true})
-    ingredients:string;
+  @Column({ array: true })
+  ingredients: string;
 
-    @Column({type:'numeric'})
-    protein: number;
+  @Column({ type: 'numeric' })
+  protein: number;
 
-    @Column({type:'numeric'})
-    carbohydrates: number;
+  @Column({ type: 'numeric' })
+  carbohydrates: number;
 
-    @Column({type:'numeric'})
-    calories: number;
+  @Column({ type: 'numeric' })
+  calories: number;
 
-    @Column({array:true})
-    mealType: string
+  @Column({ array: true })
+  mealType: string;
 }
--- src/meals/meals.controller.ts
+++ src/meals/meals.controller.ts
@@ -1,42 +1,53 @@
-import { Body, Controller, Delete, Get, Patch, Post, Req, Headers } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  Post,
+  Req,
+  Headers,
+  Param,
+  Logger,
+} from '@nestjs/common';
 import { AddMeal } from './dto/add-meal.dto';
 import { MealsService } from './meals.service';
 
 @Controller('meals')
 export class MealsController {
-    constructor(private readonly mealsService:MealsService){}
+  constructor(private readonly mealsService: MealsService) {}
 
-    @Post('add')
-    addMeal(@Body() addMeal:AddMeal, @Body() userId){
-        return this.mealsService.addMeal(addMeal,userId)
-    }
+  @Post('add')
+  addMeal(@Body() addMeal: AddMeal, @Body() userId) {
+    return this.mealsService.addMeal(addMeal, userId);
+  }
 
-    @Delete('delete')
-    deleteMeal(@Body() mealId){
-        return this.mealsService.deleteMeal(mealId)
-    }
+  @Delete('delete')
+  deleteMeal(@Body() mealId) {
+    return this.mealsService.deleteMeal(mealId);
+  }
 
-    @Post('createMealPlan')
-    createMealPlan(@Body() date, @Body() userId){
-        return this.mealsService.createMealPlan(date,userId)
-    }
+  @Post('createMealPlan')
+  createMealPlan(@Body() date, @Body() userId) {
+    return this.mealsService.createMealPlan(date, userId);
+  }
 
-    @Get('getDailyMealPlan')
-    getDailyMealPlan(@Body() date, @Body() userId){
-        return this.mealsService.getDailyMealPlan(date, userId)
-    }
+  @Get('getDailyMealPlan')
+  getDailyMealPlan(@Body() date, @Body() userId) {
+    return this.mealsService.getDailyMealPlan(date, userId);
+  }
 
-    @Get('getSpecificMealType')
-    getSpecificMeals(@Body() mealType, @Body() userId){
-        return this.mealsService.getSpecificMeals(mealType,userId)
-    }
+  @Get('getSpecificMealType')
+  getSpecificMeals(@Body() mealType, @Body() userId) {
+    return this.mealsService.getSpecificMeals(mealType, userId);
+  }
 
-    @Patch()
-    editMeal(){
-        
-    }
+  @Get('/searchMealsByKeyword/:keyword')
+  searchMealsByKeyword(@Param('keyword') keyword: string) {
+    Logger.log(`MealsController.searchMealsByKeyword(${keyword})`);
+    return this.mealsService.searchByKeyword(keyword);
+  }
 
-
-
-    
+  @Patch()
+  editMeal() {}
 }
