5285f98e669c17fb8e02818d5bd773c3c4b43711
Joachim Rives <47552793+SS-Runen@users.noreply.github.com>
2021-12-13 10:03:39 +0800

    Make spacing uniform according to prettier.

--- src/meals/entity/meal.entity.ts
+++ src/meals/entity/meal.entity.ts
@@ -3,14 +3,14 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('meals')
 export class MealEntity {
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
+    @PrimaryGeneratedColumn('uuid')
+    id:string;
 
-  @Column()
-  userId: string;
+    @Column()
+    userId:string;
 
-  @Column()
-  mealName: string;
+    @Column()
+    mealName:string;
 
     @Column({nullable:true})
     mealImage:string;
@@ -21,15 +21,15 @@ export class MealEntity {
     @Column({type: "jsonb",nullable:true})
     ingredients:string[]
 
-  @Column({ type: 'numeric' })
-  protein: number;
+    @Column({type:'numeric'})
+    protein: number;
 
-  @Column({ type: 'numeric' })
-  carbohydrates: number;
+    @Column({type:'numeric'})
+    carbohydrates: number;
 
-  @Column({ type: 'numeric' })
-  calories: number;
+    @Column({type:'numeric'})
+    calories: number;
 
-  @Column({type:"jsonb"})
-  mealType: string[]
+    @Column({type:"jsonb"})
+    mealType: string[]
 }
